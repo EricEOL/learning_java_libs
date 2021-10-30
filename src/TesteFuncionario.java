12cc25a5cced28aeb5b176078822ff287522de0c
EricEOL <lopes.eric051@gmail.com>
2021-10-30 18:25:13 -0300

    first commit

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,9 @@
+public class TesteFuncionario {
+  public static void main(String[] args) {
+    Funcionario eric = new Funcionario();
+    
+    eric.setNome("Eric Lopes");
+    eric.setCpf("123.456.789-00");
+    eric.setSalario(12500);
+  }
+}

33eb1f2c0dbc68cb54add2064d7901b48687e8b3
EricEOL <lopes.eric051@gmail.com>
2021-10-30 19:18:41 -0300

    Aprendendo a utilizar mééetodos da classe pai com super

--- src/TesteFuncionario.java
+++ src/TesteFuncionario.java
@@ -5,5 +5,9 @@ public class TesteFuncionario {
     eric.setNome("Eric Lopes");
     eric.setCpf("123.456.789-00");
     eric.setSalario(12500);
+
+    Gerente thamiris = new Gerente();
+    thamiris.setSalario(5000);
+    System.out.println(thamiris.getBonificacao());
   }
 }

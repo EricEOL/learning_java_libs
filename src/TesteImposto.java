825e020f322ad2d6dfd920131fcf249adf6ece5e
EricEOL <lopes.eric051@gmail.com>
2021-11-04 22:29:38 -0300

    Praticando interfaces

---
+++ src/TesteImposto.java
@@ -0,0 +1,15 @@
+public class TesteImposto {
+  public static void main(String[] args) {
+    ContaCorrente cc = new ContaCorrente(1111, 1111);
+    cc.deposita(3000);
+
+    SeguroDeVida segVida = new SeguroDeVida();
+
+    CalculadorImposto calculador = new CalculadorImposto();
+
+    calculador.registra(cc);
+    calculador.registra(segVida);
+
+    System.out.println(calculador.getTotalImposto());
+  }
+}

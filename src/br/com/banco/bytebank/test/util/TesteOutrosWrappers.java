721b6a189d31d76359d783132208e7b309d80479
EricEOL <lopes.eric051@gmail.com>
2021-11-18 20:20:52 -0300

    Aprendendo sobre outros Wrappers

---
+++ src/br/com/banco/bytebank/test/util/TesteOutrosWrappers.java
@@ -0,0 +1,27 @@
+package br.com.banco.bytebank.test.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteOutrosWrappers {
+
+	public static void main(String[] args) {
+		
+		Integer idadeRef = Integer.valueOf(26);
+		System.out.println(idadeRef.intValue());
+
+		Double dRef = Double.valueOf(3.2);
+		System.out.println(dRef.doubleValue());
+		
+		Boolean bRef = Boolean.TRUE;
+		System.out.println(bRef.booleanValue());
+		
+		Number refNumero = Float.valueOf(27.9f);
+		
+		List<Number> lista = new ArrayList<>();
+		lista.add(10);
+		lista.add(32.6);
+		lista.add(5.6f);
+	}
+
+}
